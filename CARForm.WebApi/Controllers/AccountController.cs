644a1738777937614c466e84a6fa8fd22461c534
hafizi <hafizi.wijaksana@celindo-pratama.com>
2024-09-20 11:46:35 +0700

    get vendor info from master data api

--- CARForm.WebApi/Controllers/AccountController.cs
+++ CARForm.WebApi/Controllers/AccountController.cs
@@ -25,11 +25,6 @@ public class AccountController(IServiceManager business, IOptions<AppSettings> a
         if (result.Item2 is not null)
         {
             SetJwtTokenCookies(result.Item2);
-            if(result.Item1.Success == true)
-            {
-                result.Item1.Content.vendorcode = result.Item2.vendorcode;
-                result.Item1.Content.vendorname = result.Item2.vendorname;
-            }
         }
 
         return Ok(result.Item1);
