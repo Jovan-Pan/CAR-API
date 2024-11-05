153b5991e2b4896f2ffad822633e2d7e8395a8b7
jovan <jovan@celindo-pratama.com>
2024-11-05 16:04:53 +0700

    Create new Master data NCTextSentence

--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -1,20 +1,22 @@
 ﻿using Contracts.Repository;
 using Contracts.Repository.CAR;
 using Contracts.Repository.MasterData;
+using Microsoft.AspNetCore.Http;
 using Repository.CAR;
 using Repository.MasterData;
 
 namespace Repository;
 
-public sealed class RepositoryManager(DbContext dbContext) : IDataManager
+public sealed class RepositoryManager(DbContext dbContext, IHttpContextAccessor httpContextAccessor) : IDataManager
 {
     private readonly Lazy<IMDMRepository> _mdmRepo = new(() => new MDMRepository(dbContext));
     private readonly Lazy<IImmidateActionRepository> _immediteActrepo = new(() => new ImmidateActionRepository(dbContext));
-    private readonly Lazy<IRootCauseRepository> _rootcauserepo = new(() => new RootCauseRepository(dbContext));
+    private readonly Lazy<IRootCauseRepository> _rootcauserepo = new(() => new RootCauseRepository(dbContext,httpContextAccessor));
     private readonly Lazy<IErrorLogRepository> _ErrorLog = new(() => new ErrorLogRepository(dbContext));
     private readonly Lazy<IIssueSubmissionRepository> _ISM = new(() => new IssueSubmissionRepository(dbContext));
     private readonly Lazy<IIssueFeedbackReportRepository> _IFR = new(() => new IssueFeedbackReportRepository(dbContext));
     private readonly Lazy<ISendMailSettingRepository> _SMS = new(() => new SendMailSettingRepository(dbContext));
+    private readonly Lazy<INCTextSentenceRepository> _NCTS = new(() => new NCTextSentenceRepository(dbContext));
     public IMDMRepository MDM => _mdmRepo.Value;
     public IImmidateActionRepository ImmAct => _immediteActrepo.Value;
     public IRootCauseRepository RootCause => _rootcauserepo.Value;
@@ -22,4 +24,5 @@ public sealed class RepositoryManager(DbContext dbContext) : IDataManager
     public IIssueSubmissionRepository ISM => _ISM.Value;
     public IIssueFeedbackReportRepository IFR => _IFR.Value;
     public ISendMailSettingRepository SMS => _SMS.Value;
+    public INCTextSentenceRepository NCTS => _NCTS.Value;
 }
