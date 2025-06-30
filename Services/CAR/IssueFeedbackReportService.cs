752d230b10eea394b35d9bb3f29d36abbd9976d4
Jovan <Jovan@celindo-pratama.com>
2025-06-30 10:28:12 +0700

    Fix Pagination Issue

--- Services/CAR/IssueFeedbackReportService.cs
+++ Services/CAR/IssueFeedbackReportService.cs
@@ -183,7 +183,7 @@ namespace Services.CAR
                     Cpr.ExtraWhereCondition += Condquery;
                 }
                 #endregion
- 
+            var FilterTtlRecord = await data.IFR.GetTotalRecord(param, Cpr);
             var maindata = await data.IFR.GetMaindata(param, Cpr);
             var formNoList = maindata.Select(data => data.FormNo);
             var dataAtch = await data.IFR.GetDataAttchment(param.Plant, formNoList,null);
@@ -334,9 +334,9 @@ namespace Services.CAR
 
                     maindata = maindataList;
                 }
-            }
+            }   
             result.maindata = maindata;
-            result.totrecord = totrecord;
+            result.totrecord = FilterTtlRecord;
             return ApiResponse<IssueFeedbackResultDto>.SuccessResponse(result);
         }
 
