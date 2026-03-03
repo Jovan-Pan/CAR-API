587e55fc459bbfeac8ec7fc6e55e57d238882d86
Kelbin <kelbin@shimano.com.sg>
2026-03-03 21:46:53 +0800

    sso initial commit

---
+++ Services/Account/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Services.Contracts.Account;
+using System.Security.Claims;
+
+namespace Services.Account;
+
+public sealed class CurrentUserService(IHttpContextAccessor httpContextAccessor, IOptions<AppSettings> appSettings) : ICurrentUserService
+{
+    public string? UserId =>
+        appSettings.Value.SSOConfig.Enable
+        ? httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+        : httpContextAccessor.HttpContext?.User?.FindFirst("UseID")?.Value;
+
+    public bool IsAuthenticated =>
+        httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+}
---
+++ Services/Helper/HMACHasher.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Services.Helper;
+
+public static class HMACHasher
+{
+    public static async Task<string> HashValue(string valueToHash, string secretKey)
+    {
+        var strStreamOne = new MemoryStream(Encoding.UTF8.GetBytes(valueToHash));
+        byte[] hashOne;
+        byte[] key = Encoding.UTF8.GetBytes(secretKey);
+        using (var hmac = new HMACSHA256(key))
+        {
+            hashOne = await hmac.ComputeHashAsync(strStreamOne);
+        }
+
+        return Convert.ToHexString(hashOne);
+    }
+}
